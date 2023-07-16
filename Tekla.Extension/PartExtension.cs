767e3d76ff9c555eeda50bbaa4bfeec9f63477d9
Anatolii Ivanov <gotnickname@gmail.com>
2023-07-16 13:21:01 +0600

    added PartExtension.cs and .ProfileTypeIs(string) method

---
+++ Tekla.Extension/PartExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace Tekla.Extension
+{
+    /// <summary>
+    /// Extension class for working with model Part class in Tekla Structures
+    /// </summary>
+    public static class PartExtension
+    {
+        
+        /// <summary>
+        /// Checks report property PROFILE_TYPE for selected value. Usefull to recognising plates made as beams.
+        /// </summary>
+        /// <param name="part">Extended class object</param>
+        /// <param name="type">String representation of Profile Type as in https://support.tekla.com/doc/tekla-structures/2023/profile_type</param>
+        /// <returns>True if profile type is correct</returns>
+        public static bool ProfileTypeIs(this Part part, string type)
+        {
+            string profType = string.Empty;
+            part.GetReportProperty("PROFILE_TYPE", ref profType);
+            return (type == profType);
+        }
+    }
+}
