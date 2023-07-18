866bdd7c419e0efe415fff00a2c13b05dd6f6fb3
Yura <yura131314@gmail.com>
2023-07-18 06:31:17 -0400

    First commit

--- Tekla.Extension/LinqExtension.cs
+++ Tekla.Extension/LinqExtension.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Tekla.Structures.Model;
 
-namespace Tekla.Extension.LinqExtension;
+namespace Tekla.Extension;
 
 /// <summary>
 /// Extension class for queries in model
