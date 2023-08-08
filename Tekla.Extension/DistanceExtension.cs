16b9cd5b23074027bb201eefbd71a1aaa14ae6b0
Yura <yura131314@gmail.com>
2023-08-07 22:05:24 -0400

    Made vertical panels and main system of beams

---
+++ Tekla.Extension/DistanceExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using TSD = Tekla.Structures.Datatype;
+
+
+namespace Tekla.Extension;
+public static class DistanceExtension
+{
+    public static IReadOnlyList<double> GetDistances(this string distances)
+    {
+        List<double> result = new();
+
+        foreach (TSD.Distance distance in TSD.DistanceList.Parse(distances, CultureInfo.InvariantCulture, TSD.Distance.CurrentUnitType))
+            result.Add(distance.ConvertTo(TSD.Distance.CurrentUnitType));
+
+        return result;
+    }
+}
