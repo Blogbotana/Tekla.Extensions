959611c5b16eac5381c712aecfee27ffae7e2dbc
Yura <yura131314@gmail.com>
2024-03-06 19:27:39 -0300

    Added new methods and bugfixes work with Arc

--- Tekla.Extension/Drawer.cs
+++ Tekla.Extension/Drawer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Internal;
 using Tekla.Structures.Model;
 using Tekla.Structures.Model.UI;
 
