331c43d058b17645fb4ea8118faad64d0967dd3b
Yura <yura131314@gmail.com>
2024-04-12 15:57:49 -0300

    Made bugfix and Component Functions

--- Tekla.Extension/GeometryExtension.cs
+++ Tekla.Extension/GeometryExtension.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tekla.Structures;
 using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
 
 namespace Tekla.Extension;
 /// <summary>
@@ -21,7 +22,7 @@ public static class GeometryExtension
         Vector testPointVector = new Vector(testPoint - startPoint);
 
         double dotProduct = Math.Round(lineVector.Dot(testPointVector), 2);
-        double magnitude = lineVector.GetLengthSquared();
+        double magnitude = Math.Round(lineVector.GetLengthSquared(), 2);
 
         if (dotProduct < 0 || dotProduct > magnitude)
             return false;
@@ -38,7 +39,7 @@ public static class GeometryExtension
         {
             int j = i == 0 ? polygon.Count - 1 : i - 1;
             Point point1 = polygon.ElementAt(i);
-            Point point2 = polygon.ElementAt(j);    
+            Point point2 = polygon.ElementAt(j);
 
             if (point1.Y <= testPoint.Y)
             {
@@ -56,9 +57,9 @@ public static class GeometryExtension
             }
 
 
-            if(includeLine)
+            if (includeLine)
             {
-                if(IsPointInLineSegment3d(point1, point2, testPoint))
+                if (IsPointInLineSegment3d(point1, point2, testPoint))
                     return true;
             }
         }
@@ -69,5 +70,73 @@ public static class GeometryExtension
     {
         return ((point2.X - point1.X) * (testPoint.Y - point1.Y)) - ((testPoint.X - point1.X) * (point2.Y - point1.Y)) > 0;
     }
+    public static Point[] GetInputPointsOfComponent(Component component)
+    {
+        List<Point> inputPoints = new();
+        ComponentInput originalInput = component.GetComponentInput();
+        if (originalInput is null)
+            return Array.Empty<Point>();
 
+
+        foreach (object inputItem in originalInput)
+        {
+            if (inputItem is not InputItem item)
+                continue;
+
+            switch (item.GetInputType())
+            {
+                case InputItem.InputTypeEnum.INPUT_1_POINT:
+                    inputPoints.Add(item.GetData() as Point);
+                    break;
+                case InputItem.InputTypeEnum.INPUT_2_POINTS or InputItem.InputTypeEnum.INPUT_POLYGON:
+                    {
+                        if (item.GetData() is not ArrayList data)
+                            break;
+
+                        inputPoints.AddRange(data.Cast<Point>());
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+        return inputPoints.ToArray();
+    }
+    public static IReadOnlyCollection<ModelObject> GetInputObjectsOfComponent(Component component)
+    {
+        var model = new Model();
+        List<ModelObject> inputObjects = new();
+        ComponentInput originalInput = component.GetComponentInput();
+        if (originalInput is null)
+            return Array.Empty<ModelObject>();
+
+        foreach (object inputItem in originalInput)
+        {
+            if (inputItem is not InputItem item)
+                continue;
+
+            switch (item.GetInputType())
+            {
+                case InputItem.InputTypeEnum.INPUT_1_OBJECT:
+                    var id = item.GetData() as Identifier;
+                    var inputObject = model.SelectModelObject(id);
+                    if (inputObject is not null && inputObject.Identifier.ID != 0)
+                        inputObjects.Add(inputObject);
+                    break;
+                case InputItem.InputTypeEnum.INPUT_N_OBJECTS:
+                    {
+                        if (item.GetData() is not ArrayList data)
+                            break;
+
+                        inputObjects.AddRange(data.Cast<Identifier>()
+                            .Select(id => model.SelectModelObject(id))
+                            .Where(o => o is not null && o.Identifier.ID != 0));
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+        return inputObjects;
+    }
 }
